3c9dffc51e4f674227ecd2a0565cf8712246b3d9
juliancoffee <lightdarkdaughter@gmail.com>
2019-11-28 00:20:35 +0200

    fixed bugs with some path

--- src/pwd.rs
+++ src/pwd.rs
@@ -1,11 +1,7 @@
 use std::path;
 use std::env;
-extern crate dirs;
 
-fn home() -> String {
-    let home = dirs::home_dir().expect("failed to get home directory");
-    home.to_str().expect("failed to parse home directore").to_string()
-}
+extern crate dirs;
 
 pub fn current_path(need_short: bool) -> String {
     let cur_dir = env::current_dir().expect("failed to get current directory");
@@ -18,25 +14,52 @@ pub fn current_path(need_short: bool) -> String {
 }
 
 fn pwd(current_dir: path::PathBuf) -> String {
-    current_dir.to_str().expect("failed to parse current path").to_string()
+    let path = current_dir.to_str().expect("failed to parse current path").to_string();
+    let home = home();
+    let home = home.to_str().expect("failed to parse home directory");
+    path.replacen(&home, "~", 1)
 }
 
 fn short_pwd(current_dir: path::PathBuf) -> String {
+    if current_dir == home() {
+        return String::from("~")
+    }
+
+    let home = home();
+    let home = home.to_str().expect("failed to parse home directory");
     let dir_sep = path::MAIN_SEPARATOR;
+    let sep: &str = &[dir_sep].iter().collect::<String>();
 
-    let head = current_dir.file_name().expect("failed to get name of directory")
-        .to_str().expect("failed to parse name of directory");
+    let head = match current_dir.file_name() {
+        Some(name) => name,
+        None => path::Path::new("").as_os_str()
+    }.to_str().expect("failed to parse name of directory");
 
-    let tail = current_dir.parent().expect("failed to get parrent path");
-    let tail = tail.to_str().expect("failed to parse parrent path");
-    let tail = tail.replacen(&home(), "~", 1);
+    let tail = match current_dir.parent() {
+        Some(name) => name,
+        None => path::Path::new("")
+    }.to_str().expect("failed to parse parrent path");
+        //.expect("failed to get parrent path");
+    //let tail = parent_path.to_str().expect("failed to parse parrent path");
+
+    let tail = tail.replacen(&home, "~", 1);
 
     let mut short_path: Vec<String> = tail.split(dir_sep)
-        .map(|s| format!("{}", s.chars().nth(0)
-                         .expect("failed to get first character of str")))
+        .map(|s| match s {
+            "" => String::from(""),
+            _ => format!("{}", s.chars().nth(0)
+                         .expect("failed to get first character of str"))
+        })
         .collect();
-    short_path.push(String::from(head));
+    if tail == String::from("/") {
+        format!("/{}", head)
+    } else {
+        short_path.push(String::from(head));
+        short_path.join(&*sep)
+    }
 
-    let sep: &str = &[dir_sep].iter().collect::<String>();
-    short_path.join(sep)
+}
+
+fn home() -> path::PathBuf {
+    dirs::home_dir().expect("failed to get home directory")
 }
